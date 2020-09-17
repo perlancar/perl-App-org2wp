fa52772edfd358ff6fc6ab69e6f4b910eb281322
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-09-17 08:56:42 +0700

    Finish supporting heading mode

--- lib/App/org2wp.pm
+++ lib/App/org2wp.pm
@@ -10,9 +10,16 @@ use strict;
 use warnings;
 use Log::ger;
 
+use Perinci::Object qw(envresmulti);
 use POSIX qw(strftime);
 
 our %SPEC;
+
+sub _fmt_timestamp_org {
+    my $time = shift;
+
+    strftime("%Y-%m-%d %a %H:%M", localtime($time));
+}
 
 $SPEC{'org2wp'} = {
     v => 1.1,
@@ -107,9 +114,9 @@ post. For example, suppose you have `blog.org` with this content:
 
     Some text...
 
-    ** a heading of post 1
+    ** a heading of post A
     more text ...
-    ** another heading of post 1
+    ** another heading of post A
     even more text ...
 
     * Post B                  :tag2:tag4:
@@ -229,7 +236,7 @@ sub org2wp {
     (-f $filename) or return [404, "No such file '$filename'"];
 
     require File::Slurper;
-    my $org_source = File::Slurper::read_text($filename);
+    my $file_content = File::Slurper::read_text($filename);
 
     my $post_heading_level = $args{post_heading_level};
     my $mode = 'document';
@@ -237,82 +244,108 @@ sub org2wp {
         $mode = 'heading';
     }
 
-    # 1. collect the posts information: the org source, along with their titles,
-    # existing post ID's (if available), as well as all categories and tags we
-    # want to use.
+    # 1. collect the posts information: for each post: the org source, its
+    # title, existing post ID (if available), as well as all categories and tags
+    # we want to use.
+
     my @posts_srcs;   # ("org1", "org2", ...)
+    my @posts_htmls;  # ("html1", "html2", ...)
     my @posts_titles; # ("title1", "title2", ...)
     my @posts_ids;    # (101,     undef,   ...)
     my @posts_tags;   # ([tag1_for_post1,tag2_for_post1], [tag1_for_post2,...], ...)
     my @posts_cats;   # ([cat1_for_post1,cat2_for_post1], [cat1_for_post2,...], ...)
+    my @posts_times;  # ("[2020-09-17 Thu 01:55]", ...)
+    my @posts_headlines; # ($headline_obj1, ...) # only for heading mode
 
-    require Org::To::HTML::WordPress;
-    if ($mode eq 'heading') {
-        require Org::Parser;
-        my $org_parser = Org::Parser->new;
-        my $org_doc   = $org_parser->parse($org_source);
+  L1_COLLECT_POSTS_INFORMATION: {
+        require Org::To::HTML::WordPress;
+        if ($mode eq 'heading') {
+            require Org::Parser;
+            my $org_parser = Org::Parser->new;
+            my $org_doc   = $org_parser->parse($file_content);
 
-        my @headlines = $org_doc->find(
-            sub {
-                my $el = shift;
-                $el->isa("Org::Element::Headline") && $el->level == $post_heading_level;
-            });
-        for my $headline (@headlines) {
-            log_trace "Found blog post in heading: %s", $headline->as_string;
-            push @posts_srcs, $headline->as_string . $headline->children_as_string;
-            # XXX posts_ids
-            # XXX posts_ids
-            # XXX post_tags
-            # XXX post_cats
-        }
-    } else {
-        push @posts_srcs, $org_source;
+            @posts_headlines = $org_doc->find(
+                sub {
+                    my $el = shift;
+                    $el->isa("Org::Element::Headline") && $el->level == $post_heading_level;
+                });
+            for my $headline (@posts_headlines) {
+                push @posts_srcs, $headline->children_as_string;
+                push @posts_titles, $headline->title->as_string;
 
-        my $title;
-        if ($org_source =~ /^#\+TITLE:\s*(.+)/m) {
-            $title = $1;
-            log_trace("Extracted title from Org document: %s", $title);
+                # find the first properties drawer
+                my ($properties_drawer) = $headline->find(
+                    sub {
+                        my $el = shift;
+                        $el->isa("Org::Element::Drawer") && $el->name eq 'PROPERTIES';
+                    });
+                my $properties = $properties_drawer ? $properties_drawer->properties : {};
+
+                push @posts_ids, $properties->{POSTID};
+
+                push @posts_tags, [$headline->get_tags];
+
+                push @posts_cats, [split /\s*,\s*/, ($properties->{CATEGORY} // $properties->{CATEGORIES} // '')];
+                log_trace "Found blog post in heading, title=%s, ID=%d, tags=%s, cats=%s",
+                    $posts_titles[-1],
+                    $posts_ids[-1],
+                    $posts_tags[-1],
+                    $posts_cats[-1];
+            }
         } else {
-            $title = "(No title)";
+            push @posts_srcs, $file_content;
+
+            my $title;
+            if ($file_content =~ /^#\+TITLE:\s*(.+)/m) {
+                $title = $1;
+                log_trace("Extracted title from Org document: %s", $title);
+            } else {
+                $title = "(No title)";
+            }
+            push @posts_titles, $title;
+
+            my $post_tags;
+            if ($file_content =~ /^#\+TAGS?:\s*(.+)/m) {
+                $post_tags = [split /\s*,\s*/, $1];
+                log_trace("Extracted tags from Org document: %s", $post_tags);
+            } else {
+                $post_tags = [];
+            }
+            push @posts_tags, $post_tags;
+
+            my $post_cats;
+            if ($file_content =~ /^#\+CATEGOR(?:Y|IES):\s*(.+)/m) {
+                $post_cats = [split /\s*,\s*/, $1];
+                log_trace("Extracted categories from Org document: %s", $post_cats);
+            } else {
+                $post_cats = [];
+            }
+            push @posts_cats, $post_cats;
+
+            my $post_id;
+            if ($file_content =~ /^#\+POSTID:\s*(\d+)/m) {
+                $post_id = $1;
+                log_trace("Org document already has post ID: %s", $post_id);
+            }
+            push @posts_ids, $post_id;
         }
-        push @posts_titles, $title;
+    } # L1_COLLECT_POSTS_INFORMATION
 
-        my $post_tags;
-        if ($org =~ /^#\+TAGS?:\s*(.+)/m) {
-            $post_tags = [split /\s*,\s*/, $1];
-            log_trace("Extracted tags from Org document: %s", $post_tags);
-        } else {
-            $post_tags = [];
-        }
-        push @posts_tags, $post_tags;
+    # 2. convert the org sources to htmls
 
-        my $post_cats;
-        if ($org =~ /^#\+CATEGOR(?:Y|IES):\s*(.+)/m) {
-            $post_cats = [split /\s*,\s*/, $1];
-            log_trace("Extracted categories from Org document: %s", $post_cats);
-        } else {
-            $post_cats = [];
-        }
-        push @posts_cats, $post_cats;
-
-        my $post_id;
-        if ($org =~ /^#\+POSTID:\s*(\d+)/m) {
-            $post_id = $1;
-            log_trace("Org document already has post ID: %s", $post_id);
-        }
-        push @posts_ids, $post_id;
-    }
-
-    # 2. convert hte org sources to htmls
-
-    log_info("Converting Org to HTML ...");
-    my $res = Org::To::HTML::WordPress::org_to_html_wordpress(
-        source_file => $filename,
-        naked => 1,
-        ignore_unknown_settings => 1,
-    );
-    return [500, "Can't convert Org to HTML: $res->[0] - $res->[1]"]
-        if $res->[0] != 200;
+  L2_CONVERT_ORG_TO_HTML:
+    for my $post_idx (0 .. $#posts_srcs) {
+        log_info("Converting Org%s to HTML ...",
+                 $mode eq 'heading' ? "[$post_idx]" : '');
+        my $res = Org::To::HTML::WordPress::org_to_html_wordpress(
+            source_str => $posts_srcs[$post_idx],
+            naked => 1,
+            ignore_unknown_settings => 1,
+        );
+        return [500, "Can't convert Org[$post_idx] to HTML: $res->[0] - $res->[1]"]
+            if $res->[0] != 200;
+        push @posts_htmls, $res->[2];
+    } # L2_CONVERT_ORG_TO_HTML
 
     require XMLRPC::Lite;
     my $call;
@@ -320,8 +353,8 @@ sub org2wp {
     # 3. create categories if necessary
 
     my $cat_ids = {};
-    {
-        log_info("[api] Listing categories ...");
+    L3_CREATE_CATEGORIES: {
+        log_info("[api] Listing all known categories ...");
         $call = XMLRPC::Lite->proxy($args{proxy})->call(
             'wp.getTerms',
             1, # blog id, set to 1
@@ -331,15 +364,16 @@ sub org2wp {
         );
         return [$call->fault->{faultCode}, "Can't list categories: ".$call->fault->{faultString}]
             if $call->fault && $call->fault->{faultCode};
-        my $all_cats = $call->result;
-        for my $cat (@$post_cats) {
-            if (my ($cat_detail) = grep { $_->{name} eq $cat } @$all_cats) {
+        my $all_known_cats = $call->result;
+        for my $cat (map { @$_ } @posts_cats) {
+            if (my ($cat_detail) = grep { $_->{name} eq $cat } @$all_known_cats) {
                 $cat_ids->{$cat} = $cat_detail->{term_id};
-                log_trace("Category %s already exists", $cat);
+                log_trace("Category %s already exists with ID %d, will not be recreating",
+                          $cat, $cat_detail->{term_id});
                 next;
             }
             if ($dry_run) {
-                log_info("(DRY_RUN) [api] Creating category %s ...", $cat);
+                log_info("[DRY-RUN] [api] Creating category %s ...", $cat);
                 next;
             }
             log_info("[api] Creating category %s ...", $cat);
@@ -352,15 +386,16 @@ sub org2wp {
              );
             return [$call->fault->{faultCode}, "Can't create category '$cat': ".$call->fault->{faultString}]
                 if $call->fault && $call->fault->{faultCode};
+            push @$all_known_cats, { name=>$cat, term_id=>$call->result };
             $cat_ids->{$cat} = $call->result;
         }
-    }
+    } # L3_CREATE_CATEGORIES
 
     # 4. create tags if necessary
 
     my $tag_ids = {};
-    {
-        log_info("[api] Listing tags ...");
+    L4_CREATE_TAGS: {
+        log_info("[api] Listing all known tags ...");
         $call = XMLRPC::Lite->proxy($args{proxy})->call(
             'wp.getTerms',
             1, # blog id, set to 1
@@ -370,11 +405,12 @@ sub org2wp {
         );
         return [$call->fault->{faultCode}, "Can't list tags: ".$call->fault->{faultString}]
             if $call->fault && $call->fault->{faultCode};
-        my $all_tags = $call->result;
-        for my $tag (@$post_tags) {
-            if (my ($tag_detail) = grep { $_->{name} eq $tag } @$all_tags) {
+        my $all_known_tags = $call->result;
+        for my $tag (map { @$_ } @posts_tags) {
+            if (my ($tag_detail) = grep { $_->{name} eq $tag } @$all_known_tags) {
                 $tag_ids->{$tag} = $tag_detail->{term_id};
-                log_trace("Tag %s already exists", $tag);
+                log_trace("Tag %s already exists with ID %d, will not be recreating",
+                          $tag, $tag_detail->{term_id});
                 next;
             }
             if ($dry_run) {
@@ -391,13 +427,22 @@ sub org2wp {
              );
             return [$call->fault->{faultCode}, "Can't create tag '$tag': ".$call->fault->{faultString}]
                 if $call->fault && $call->fault->{faultCode};
+            push @$all_known_tags, { name=>$tag, term_id=>$call->result };
             $tag_ids->{$tag} = $call->result;
         }
-    }
+    } # L4_CREATE_TAGS
 
     # 5. create or edit the posts
 
-    {
+    my $envres = envresmulti();
+  L5_CREATE_OR_EDIT_POSTS:
+    for my $post_idx (0..$#posts_srcs) {
+        my $post_html  = $posts_htmls [$post_idx];
+        my $post_title = $posts_titles[$post_idx];
+        my $post_id    = $posts_ids   [$post_idx];
+        my $post_tags  = $posts_tags  [$post_idx];
+        my $post_cats  = $posts_cats  [$post_idx];
+
         my $meth;
         my @xmlrpc_args = (
             1, # blog id, set to 1
@@ -411,13 +456,13 @@ sub org2wp {
             strftime("%Y%m%dT%H:%M:%S", localtime($args{schedule})) : undef;
         $publish = 1 if $schedule;
 
-        if ($postid) {
+        if ($post_id) {
             $meth = 'wp.editPost';
             $content = {
                 (post_status => $publish ? 'publish' : 'draft') x !!(defined $publish),
                 (post_date => $schedule) x !!(defined $schedule),
-                post_title => $title,
-                post_content => $res->[2],
+                post_title => $post_title,
+                post_content => $post_html,
                 terms => {
                     category => [map {$cat_ids->{$_}} @$post_cats],
                     post_tag => [map {$tag_ids->{$_}} @$post_tags],
@@ -425,14 +470,14 @@ sub org2wp {
                 comment_status => $args{comment_status},
                 %{ $args{extra_attrs} // {} },
             };
-            push @xmlrpc_args, $postid, $content;
+            push @xmlrpc_args, $post_id, $content;
         } else {
             $meth = 'wp.newPost';
             $content = {
                 post_status => $publish ? 'publish' : 'draft',
                 (post_date => $schedule) x !!(defined $schedule),
-                post_title => $title,
-                post_content => $res->[2],
+                post_title => $post_title,
+                post_content => $post_html,
                 terms => {
                     category => [map {$cat_ids->{$_}} @$post_cats],
                     post_tag => [map {$tag_ids->{$_}} @$post_tags],
@@ -444,7 +489,9 @@ sub org2wp {
         }
         if ($dry_run) {
             log_info("(DRY_RUN) [api] Create/edit post, content: %s", $content);
-            return [304, "Dry-run"];
+            $posts_times[$post_idx] = _fmt_timestamp_org(time());
+            $posts_ids  [$post_idx] = 9_999_000 + $post_idx;
+            next L5_CREATE_OR_EDIT_POSTS;
         }
 
         log_info("[api] Creating/editing post ...");
@@ -452,19 +499,92 @@ sub org2wp {
         $call = XMLRPC::Lite->proxy($args{proxy})->call($meth, @xmlrpc_args);
         return [$call->fault->{faultCode}, "Can't create/edit post: ".$call->fault->{faultString}]
             if $call->fault && $call->fault->{faultCode};
-    }
+
+        $posts_times[$post_idx] = _fmt_timestamp_org(time());
+        $posts_ids  [$post_idx] //= $call->result;
+    } # L5_CREATE_OR_EDIT_POSTS
 
     # 6. insert #+POSTID/:POSTID: and #+POSTTIME/:POSTTIME: to Org
     # document/heading
 
-    unless ($postid) {
-        $postid = $call->result;
-        $org =~ s/^/#+POSTID: $postid\n/;
-        log_info("[api] Inserting #+POSTID to %s ...", $filename);
-        File::Slurper::write_text($filename, $org);
-    }
+  L6_INSERT_POST_IDS: {
+        my $do_update_file;
+        if ($mode eq 'heading') {
+            for my $post_idx (0..$#posts_srcs) {
+                my $post_headline = $posts_headlines[$post_idx];
+                my ($properties_drawer) = $post_headline->find(
+                    sub {
+                        my $el = shift;
+                        $el->isa("Org::Element::Drawer") && $el->name eq 'PROPERTIES';
+                    });
+                if ($properties_drawer) {
+                    # XXX need to enhance Org::Parser API, no API to add/modify/delete properties
+                    my $raw_content = $properties_drawer->children_as_string;
+                    log_info("Inserting/updating :POSTTIME: to post[%d] ...", $post_idx);
+                    $raw_content =~ s/^:POSTTIME:.*/:POSTTIME: $posts_times[$post_idx]/m
+                        or $raw_content =~ s/^/:POSTTIME: $posts_times[$post_idx]\n/;
+                    log_info("Inserting/updating :POSTID: to post[%d] ...", $post_idx);
+                    $raw_content =~ s/^:POSTID:.*/:POSTID: $posts_ids[$post_idx]/m
+                        or $raw_content =~ s/^/:POSTID: $posts_ids[$post_idx]\n/;
+                    $properties_drawer->children([]);
+                    $properties_drawer->document->_add_text($raw_content, $properties_drawer, 2);
+                    $properties_drawer->_parse_properties($raw_content);
+                } else {
+                    require Org::Element::Drawer;
+                    # XXX need to fix Org::Parser API, this is ugly
+                    my $raw_content = "";
+                    log_info("Inserting :POSTTIME: & :POSTID: to post[%d] ...", $post_idx);
+                    $raw_content .= ":POSTID: $posts_ids[$post_idx]\n";
+                    $raw_content .= ":POSTTIME: $posts_times[$post_idx]\n";
+                    $properties_drawer = Org::Element::Drawer->new(
+                        document => $post_headline->document,
+                        parent => $post_headline,
+                        name => 'PROPERTIES',
+                        pass => 2,
+                    );
+                    $post_headline->document->_add_text($raw_content, $properties_drawer, 2);
+                    $properties_drawer->_parse_properties($raw_content);
+                    my $nl = Org::Element::Text->new(
+                        document => $post_headline->document,
+                        parent => $post_headline,
+                        text => "\n",
+                    );
+                    unshift @{ $post_headline->children }, $properties_drawer, $nl;
+                }
+            } # for post
+            if (@posts_headlines) {
+                $file_content = $posts_headlines[0]->document->as_string;
+                $do_update_file++;
+            }
+        } else {
+            $do_update_file++;
 
-    [200, "OK"];
+            log_info("Inserting/updating #+POSTTIME ...", $filename);
+            s/^#\+POSTTIME:.*/#+POSTTIME: $posts_times[0]/m or
+                s/^/#+POSTTIME: $posts_times[0]\n/;
+
+            my $post_id = $posts_ids[0];
+            unless ($post_id) {
+                $post_id = $call->result;
+                log_info("Inserting #+POSTID (%d) ...", $post_id);
+                $file_content =~ s/^/#+POSTID: $post_id\n/;
+            }
+        }
+
+        if (!$do_update_file) {
+            log_info("Not updating %s because there are no changes", $filename);
+        } else {
+            log_trace("Updated file content: <<<%s>>>", $file_content);
+            if ($dry_run) {
+                log_info("[DRY-RUN] Writing %s", $filename);
+            } else {
+                log_info("Updating file %s ...", $filename);
+                File::Slurper::write_text($filename, $file_content);
+            }
+        }
+    } # L6_INSERT_POST_IDS
+
+    [$dry_run ? 304:200, "OK"];
 }
 
 1;
